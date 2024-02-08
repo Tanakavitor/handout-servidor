78d338274e58db48519ffc8715943ed218cd36c7
Vitor <seuemail>
2024-02-08 09:41:42 -0300

    aiconando code 404

--- servidor.py
+++ servidor.py
@@ -8,7 +8,7 @@ SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8080
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 server_socket.listen()
 
@@ -29,7 +29,7 @@ while True:
     elif route == '':
         response = index(request)
     else:
-        response = build_response()
+        response = build_response(code=404, reason='Not Found')
 
     client_connection.sendall(response)
 
